a2a02e6eac89029fc507c1497f4c32613698c24d
emjo91 <3mle33@gmail.com>
2018-08-10 11:15:11 -0500

    trying to make button work...

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import _ from 'lodash';
 import './style.css';
+import printMe from './print.js';
 
 function component() {
 	let element = document.createElement('div');
+	var btn = document.createElement('button');
 
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 	element.classList.add('hello');
+
+	btn.innerHTML = 'Click me and check the console!';
+	btn.onclick = printMe;
 
 	return element;
 }
