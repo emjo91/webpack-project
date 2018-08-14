3a13d6110ed4f6b9a9cc0130c6721bcef45e3770
emjo91 <3mle33@gmail.com>
2018-08-14 07:53:58 -0500

    watch is working...trying to get start to work...

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,6 @@ module.exports = {
     print: './src/print.js'
   },
   devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist'
-  },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
